aac1310fb891ff110accaf01533d3f7d535d7d55
Leonardo Ramirez <juanlov4321@gmail.com>
2024-10-18 22:41:58 -0500

    💄 Update page title and add theme attribute to html tag

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,24 @@ export default {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        pgAtlasTheme: {
+          "primary": "#3AAA3E", // Verde
+          "secondary": "#E3EDC7", // Crema claro
+          "accent": "#057489", // Azul oscuro
+          "neutral": "#22211F", // Negro oscuro
+          "base-100": "#ffffff", // Blanco como base
+          "info": "#5EB4D0", // Azul claro
+          "success": "#3AAA3E", // Verde (reutilizado para éxito)
+          "warning": "#C4A15F", // Un tono más cálido, basado en la paleta
+          "error": "#939392", // Gris (ajustado como error)
+        },
+      },
+    ],
+  }
+  ,
   plugins: [
     require('daisyui'),
   ],
