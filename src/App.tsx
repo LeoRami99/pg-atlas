1d28f0fc45142c7c67f8d5621f5b0b4b98c2173d
Leonardo Ramirez <juanlov4321@gmail.com>
2024-10-18 22:42:54 -0500

    ♻️ Refactor App component to add Map component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import Navbar from './components/Navbar'
+import Map from './components/Map'
 
 function App() {
 
   return (
     <>
       <Navbar />
-      <h1>Hola mundo</h1>
+      <Map />
     </>
   )
 }
