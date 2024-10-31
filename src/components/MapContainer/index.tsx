ea689104de42359b9c6681dd2a8ae08de4288504
Leonardo Ramirez <juanlov4321@gmail.com>
2024-10-30 23:29:43 -0500

    ✨ Refactor MapContainer to integrate API for project filtering and enhance project selection handling

--- src/components/MapContainer/index.tsx
+++ src/components/MapContainer/index.tsx
@@ -1,32 +1,43 @@
 import { useState, useEffect } from 'react';
 import MapComponent from '../MapComponent';
 import FiltersComponent from '../FiltersComponent';
-// import ProjectModal from './ProjectModal';
 import DataGoods from '../../DataGoodsUbi.json';
 import { Project } from '../../models/projects.models';
+import { getProjectsFilters } from '../../services/projects.service';
+import { Filters } from '../../types/filters.type';
+
+import ProjectModal from '../ModalProject';
 
 const MapContainer = () => {
     const [userLocation, setUserLocation] = useState<{ latitude: number; longitude: number } | null>(null);
-    const [filteredProjects, setFilteredProjects] = useState<Project[]>(DataGoods as Project[]);
+    const [filteredProjects, setFilteredProjects] = useState<Project[]>([]);
     const [selectedProject, setSelectedProject] = useState<Project | null>(null);
-
+    const filters: Filters = {
+        blockchain: '',
+        activityStatus: '',
+        country: '',
+        city: '',
+    };
     useEffect(() => {
         navigator.geolocation.getCurrentPosition((position) => {
             setUserLocation({
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude,
             });
+            getProjectsFilters(filters).then((response) => setFilteredProjects(response.data));
         });
     }, []);
 
-    const applyFilters = (filters: { city?: string; country?: string; blockchain?: string }) => {
-        const filtered = DataGoods.filter((project: Project) => {
-            return (!filters.city || project.city === filters.city) &&
-                (!filters.country || project.country === filters.country) &&
-                (!filters.blockchain || project.blockchain === filters.blockchain);
-        });
-        setFilteredProjects(filtered);
+    const applyFilters = async (filters: Filters) => {
+        const filtered = getProjectsFilters(filters).then((response) => response.data);
+        setFilteredProjects(await filtered);
     };
+
+    const handleSelectProject = (project: Project) => {
+        setSelectedProject(project);
+    };
+
+
 
     return (
         <div>
@@ -36,14 +47,11 @@ const MapContainer = () => {
                     <MapComponent
                         userLocation={userLocation}
                         projects={filteredProjects}
-                        onSelectProject={setSelectedProject}
+                        onSelectProject={handleSelectProject}
                     />
                     {selectedProject && (
-                        <div className="fixed top-0 left-0 w-full h-full bg-black/50 flex justify-center items-center">
-                            {/* <ProjectModal project={selectedProject} onClose={() => setSelectedProject(null)} /> */}
-                        </div>
+                        <ProjectModal project={selectedProject} onClose={() => setSelectedProject(null)} />
                     )}
-
                 </>
             ) : (
                 <div className='flex flex-col items-center justify-center h-screen bg-gradient-to-b from-green-200 to-green-600 text-white'>
@@ -56,7 +64,6 @@ const MapContainer = () => {
                         We need your permission to access your location in order to provide the best experience possible. Thank you for your patience!
                     </p>
                 </div>
-
             )}
         </div>
     );
