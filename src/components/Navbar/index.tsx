40bd0fe2213ec26d1d3eef65cbfe6f9942034e30
Leonardo Ramirez <juanlov4321@gmail.com>
2024-10-30 20:45:57 -0500

    🔄 Update Navbar styles for improved visibility and contrast

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,10 +5,10 @@ const Navbar = () => {
     return (
         <div className="md:w-3/4 w-full fixed z-10 flex items-center transition-[width] duration-300 md:space-x-10">
             <Menu />
-            <div className="navbar top-0 md:mt-5 backdrop-blur bg-white/30 md:border-[1px] shadow-xl rounded-badge transition-[width] duration-300  m-2">
+            <div className="navbar top-0 md:mt-5 backdrop-blur bg-black/20  shadow-xl rounded-badge transition-[width] duration-300  m-2">
                 <div className="flex-1">
                     <img src="/logoPgAtlas.png" alt="Logo PGAtlas" className="w-16" />
-                    <a className="text-xl font-bold">PGAtlas</a>
+                    <a className="text-xl font-bold text-white">PGAtlas</a>
                 </div>
                 <div className="flex-none">
                     <TonConnectButton />
