87f1dc8b805f38bf31bd43ae56238781435ac009
Leonardo Ramirez <juanlov4321@gmail.com>
2024-10-30 23:29:37 -0500

    ✨ Refactor Modal component to simplify structure and enhance styling

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,20 +1,15 @@
 import { ComponentProps } from "react";
-import Button from "../Button";
-import { IoIosCloseCircle } from "react-icons/io";
+// import Button from "../Button";
+// import { IoIosCloseCircle } from "react-icons/io";
 type ModalProps = ComponentProps<'dialog'> & {
     children: React.ReactNode;
 };
 
 const Modal = ({ ...props }: ModalProps) => {
     return (
-        <dialog className="modal" {...props}>
-            <div className="modal-box">
+        <dialog className={`modal backdrop-blur`} {...props}>
+            <div className={`modal-box`}>
                 {props.children}
-                <div className="modal-action">
-                    <form method="dialog">
-                        <Button className="btn rounded-badge bg-red-500 text-white"><IoIosCloseCircle />Close</Button>
-                    </form>
-                </div>
             </div>
         </dialog>
     );
