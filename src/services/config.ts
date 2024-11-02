a6032865056fc4a41a02f03dc9740ce484112068
Leonardo Ramirez <juanlov4321@gmail.com>
2024-11-02 00:32:32 -0500

    ✨ Update axios base URL for service configuration

--- src/services/config.ts
+++ src/services/config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL: "https://pg-atlas-back-production.up.railway.app/",
+    baseURL: "https://5v2kv694-3000.use2.devtunnels.ms",
     headers: {
         "Content-Type": "application/json",
 
